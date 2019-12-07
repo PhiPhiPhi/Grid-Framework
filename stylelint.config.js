dd0f73267593a5ec18a80f4a7ef0273415f9ee7c
Philippe Odoubadé <philippejide@outlook.com>
2019-12-07 18:11:39 +0100

    fix scss stickler errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,30 @@
 module.exports = {
   "extends": "stylelint-config-recommended",
   "rules": {
+
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    "at-rule-empty-line-before": [
+      "always", {
+        "ignoreAtRules": [ "else" ]
+      }
+    ],
+    "block-opening-brace-space-before": "always",
+    "block-closing-brace-newline-after": [
+      "always", {
+        "ignoreAtRules": [ "if", "else" ]
+      }
+    ],
+    "at-rule-name-space-after": "always",
+    "rule-empty-line-before": "always",
+    "scss/at-else-closing-brace-newline-after": "always-last-in-chain",
+    "scss/at-else-closing-brace-space-after": "always-intermediate",
+    "scss/at-else-empty-line-before": "never",
+    "scss/at-if-closing-brace-newline-after": "always-last-in-chain",
+    "scss/at-if-closing-brace-space-after": "always-intermediate"
+  },
     "at-rule-empty-line-before": [ "always", {
       except: [
         "blockless-after-same-name-blockless",
